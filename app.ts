bd84cd8df334a32dbb60eab4f0b4ca514afea9d2
zenteno125 <zenteno125gamer@gmail.com>
2023-11-18 10:20:31 -0600

    Register-create user-delete-update weight, update password(all funtional crud)

---
+++ app.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { Signale } from 'signale';
+import { userRouter } from './src/usuarios/infrastructure/userRoutes';
+
+const app = express();
+const signale = new Signale();
+
+app.use(express.json());
+
+// Rutas relacionadas con usuarios
+app.use('/users', userRouter);
+
+
+app.listen(3006, () => {
+    signale.success("Server online in port 3006");
+});
---
+++ src/usuarios/domain/userRepository.ts
@@ -0,0 +1,33 @@
+import { User } from "./user";
+
+export interface IUserRepository {
+  registerUser(
+    name: string,
+    email: string,
+    password: string, // Debe almacenarse de forma segura (hash + salt)
+    height: number,
+    weight: number,
+    sex: string,
+  ): Promise<User | null>;
+
+  listAllUsers(): Promise<User[] | null>;
+
+  deleteUserById(id: string): Promise<string | null>;
+
+  getUserById(id: string): Promise<User | null>;
+
+  listAllInactiveUser(): Promise<User[] | User | null>;
+
+  updatePassword(id: number, password: string, cpassword: string): Promise<User | null> 
+
+  setAsInactive(id: number): Promise<number | null>;
+  
+
+
+  updateUsers(
+    id: number,
+    weight: number
+): Promise<User | null>
+
+
+}
