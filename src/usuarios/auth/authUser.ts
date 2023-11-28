6ca579a3c1952f8ffa2924ee15717648697eb546
zenteno125 <zenteno125gamer@gmail.com>
2023-11-28 00:39:00 -0600

    first commit tags

--- src/usuarios/auth/authUser.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-    id: number;
-    username: string;
-    email: string;
-    password: string; // falta pasarlo con hash
-  }
--- src/usuarios/auth/domain/authRepository.ts
+++
@@ -1,5 +0,0 @@
-export interface AuthRepository {
-    login(email:string,password:string):Promise<any | null>
-    logout(logoutStatus:boolean):Promise<void>
-  }
-  
